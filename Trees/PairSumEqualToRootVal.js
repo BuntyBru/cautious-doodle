34204648ae7d096edcc643dfd587024317de3d2a
BuntyBru <buntybru@gmail.com>
2021-01-17 21:27:58 +0530

    changes in test case

--- Trees/PairSumEqualToRootVal.js
+++ Trees/PairSumEqualToRootVal.js
@@ -64,10 +64,14 @@ class bst {
 }
 
 let tree = new bst();
-tree.insert(10);
-tree.insert(6);
-tree.insert(15);
-tree.insert(2);
 tree.insert(8);
-tree.insert(20);
+tree.insert(5);
+tree.insert(4);
+tree.insert(7);
+tree.insert(1);
+tree.insert(12);
+tree.insert(2);
+tree.insert(11);
+tree.insert(3);
+
 console.log(tree.dfsPreOrder());
