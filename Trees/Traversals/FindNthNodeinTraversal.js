2a89482dc7920a038c099a274efaa46df2adfcdd
BuntyBru <buntybru@gmail.com>
2021-01-18 17:31:35 +0530

    finding the nth element in bst bfs traversal

---
+++ Trees/Traversals/FindNthNodeinTraversal.js
@@ -0,0 +1,72 @@
+/**
+ * Find nth node in Traversals
+ * https://www.geeksforgeeks.org/find-n-th-node-inorder-traversal/
+ */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+class bst {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+      if (value > current.value) {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  bfs() {
+    if (!this.root) return undefined;
+    let data = [];
+    let queue = [];
+    let node = this.root;
+    queue.push(node);
+    while (queue.length > 0) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  dfsPreOrder() {}
+}
+
+let tree = new bst();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.insert(30);
+
+console.log("3rd element in BFS", tree.bfs(), tree.bfs()[2]);
